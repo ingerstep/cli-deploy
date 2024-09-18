c24aa82e0ed91d7d352d1b19e197cdbfca0d057c
Stepan <stepan.karelin.1996@mail.ru>
2024-09-18 18:05:59 +0700

    deploy

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ const app = express();
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT | 5432,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-  }
+  connection: process.env.DATABASE_PUBLIC_URL,
+  debug: true,
 });
 
 //DB actions
--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,7 @@ configDotenv();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export const client = "pg";
-export const connection = {
-  host: process.env.PGHOST,
-  port: process.env.PGPORT | 5432,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-}
+export const connection = process.env.DATABASE_PUBLIC_URL
 export const migrations = {
   tableName: "knex_migrations",
 };
