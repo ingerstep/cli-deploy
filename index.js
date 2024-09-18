52e69444f8d7f34684cd96e56eee993ccac94a6f
Stepan <stepan.karelin.1996@mail.ru>
2024-09-18 15:55:09 +0700

    update(db): update pg

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ const app = express();
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
+  connection: process.env.DATABASE_URL,
 });
 
 //DB actions
